fcbd6a71727f4ce93e9fba318bae559f6ff0f6d3
Harsh Kumar Gupta <111433732+imguptaharsh@users.noreply.github.com>
2023-09-02 23:15:21 +0530

    add authorization on middleware

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,58 +1,71 @@
-const asyncHandler=require('express-async-handler');
-const User= require('../models/user');
-const generateToken=require('../config/generateToken');
+const asyncHandler = require("express-async-handler");
+const User = require("../models/user");
+const generateToken = require("../config/generateToken");
 
-exports.registerUser=asyncHandler(async(req,res)=>{
-    const {name,email,password,pic}=req.body;
-    console.log(req.body);
+exports.registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+  console.log(req.body);
 
-    if(!name || !email || !password){
-        res.status(400);
-        throw new Error('Please enter all fields');
-    }
-    const userExists= await User.findOne({email})
-    if(userExists){
-        res.status(400);
-        throw new Error('User already exists');
-    }
-    const user=await User.create({
-        name,
-        email,
-        password,
-        pic
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please enter all fields");
+  }
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: User.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+      message: "User Registered Successfully",
     });
-    if(user){
-        res.status(201).json({
-            _id:user._id,
-            name:User.name,
-            email:user.email,
-            pic:user.pic,
-            token:generateToken(user._id),
-            message:'User Registered Successfully'
-        });
-    }
-    else{
-          res.status(400);
-          throw new Error('Invalid user data');
-    }
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
-exports.authUser= asyncHandler(async(req,res)=>{
-    const{email,password}=req.body;
+exports.authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-    const user= await User.findOne({email});
-    if(user && (await user.matchPassword(password))){
-        res.json({
-            _id:user._id,
-            name:user.name,
-            email:user.email,
-            password:user.password,
-            token:generateToken(user._id),
-            message:'User Logged In Successfully'
-        })
-    } else{
-        res.status(400);
-        throw new Error('Invalid email or password');
-    }
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      token: generateToken(user._id),
+      message: "User Logged In Successfully",
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
 });
 
+// /api/user?search=abc    // search query
+exports.getAllUsers = asyncHandler(async (req, res) => {
+  const search = req.query.search
+//   console.log(search);
+//   res.send(search);
+  ? {
+  $or: [
+      { name: { $regex: req.query.search, $options: "i" } },
+      { email: { $regex: req.query.search, $options: "i" } },
+    ]}
+    : {};
+    const users=await User.find(search);
+    res.send({users});
+});
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router= express.Router();
 const User= require('../controllers/user');
-
-router.post('/',User.registerUser)
-    .post('/login/',User.authUser);
-
+const {protect}= require('../middleware/protect');
+router
+    .post('/',User.registerUser)
+    .post('/login/',User.authUser)
+    .get('/',User.getAllUsers)
 module.exports=router; 
